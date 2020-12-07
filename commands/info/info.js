f2d1f0f1e4e0578f8ee051204083ca3b8bd3352f
Maurizio Mangione <granzebru@gmail.com>
2020-12-07 18:41:22 +0100

    add contento to the info command

--- commands/info/info.js
+++ commands/info/info.js
@@ -7,18 +7,25 @@ module.exports = class InfoCommand extends Commando.Command {
       name: 'info',
       group: 'info',
       memberName: 'info',
-      description: 'Restituisce delle informazioni sul server'
+      description: 'Restituisce delle informazioni su questo bot'
     })
   }
 
   async run (message) {
     const embed = new MessageEmbed()
       .setColor('#ffe000')
-      .setTitle(message.guild.name)
-      .addFields(
-        {name: 'Totale membri:', value: message.guild.memberCount}
-      )
+      .setTitle('🤖 Vanilla - il bot di Italia JS')
+      .setDescription(`Vanilla è un bot nato con l'idea di essere sviluppato dai membri della community ed è stato creato in node con [Discord.js](https://discord.js.org).
+
+      Essendo [open source](https://github.com/italia-js/vanilla) ognuno può proporre e implementare nuove feature o migliorare quelle esistenti.
+      Tuttavia, se ti interessa essere coinvolto maggiormente, chiedi a <@458989281126645771> di essere ammesso nel team. Riceverai un ruolo e colore utente speciale, l'accesso al canale privato #vanilla-dev e la gratitudine eterna della community.
+
+      Sviluppare comandi per Vanilla è divertente e può essere un modo per avvicinarsi all'open source.
+
+      Aspetto i tuoi contributi! 🦾
+      `)
 
     message.channel.send(embed);
+    console.log()
   }
 }
